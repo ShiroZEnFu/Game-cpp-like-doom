5b3148a3ec79c802b05b733bf08af597473cc9c5
Dmytro Stahalski <stahalskijdima@gmail.com>
2025-02-07 13:26:13 +0100

    1.00.v1

---
+++ Game.cpp
@@ -0,0 +1,213 @@
+﻿#include <iostream>
+#include <chrono>
+#include <Windows.h>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int nScreenWidth = 120;
+int nScreenHeight = 40;
+
+float fPlayerX = 6.0f;
+float fPlayerY = 6.0f;
+float fPlayerA = 0.0f;
+
+int nMapHeight = 16;
+int nMapWidth = 16;
+
+float fFOV = 3.14159 / 4.0;
+float fDepth = 16.0f;	
+
+bool SetConsoleSize(const COORD size) {
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	if (hConsole == INVALID_HANDLE_VALUE) {
+		return false;
+	}
+
+	if (!SetConsoleScreenBufferSize(hConsole, size)) {
+		return false;
+	}
+
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	if (!GetConsoleScreenBufferInfo(hConsole, &csbi)) {
+		return false;
+	}
+
+	SMALL_RECT sr;
+	sr.Left = 0;
+	sr.Top = 0;
+	sr.Right = size.X - 1;
+	sr.Bottom = size.Y - 1;
+
+	if (!SetConsoleWindowInfo(hConsole, TRUE, &sr)) {
+		return false;
+	}
+
+	return true;
+}
+int main()
+{
+	
+	
+	COORD coord{ nScreenWidth, nScreenHeight };
+	SetConsoleSize(coord);
+	wchar_t* screen = new wchar_t[nScreenWidth * nScreenHeight];
+	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+	SetConsoleActiveScreenBuffer(hConsole);
+	DWORD dwBytesWritten = 0;
+	
+
+
+
+	wstring map;
+
+	map += L"################";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#......#.......#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#.........######";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"################";
+
+	auto tp1 = chrono::system_clock::now();
+	auto tp2 = chrono::system_clock::now();
+
+	// GameLoop
+	while (true) {
+
+		//fps
+		tp2 = chrono::system_clock::now();
+		chrono::duration<float> elapsedTime = tp2 - tp1;
+		tp1 = tp2;
+		float fElapsedTime = elapsedTime.count();
+
+
+
+		//Controls
+		if (GetAsyncKeyState((unsigned short)'A') & 0x8000) {
+			fPlayerA -= (1.0f) * fElapsedTime;
+		}
+		if (GetAsyncKeyState((unsigned short)'D') & 0x8000) {
+			fPlayerA += (1.0f) * fElapsedTime;
+		}
+		if (GetAsyncKeyState((unsigned short)'W') & 0x8000) {
+			fPlayerX += sinf(fPlayerA) * 5.0f *	fElapsedTime;
+			fPlayerY += cosf(fPlayerA) * 5.0f * fElapsedTime;
+			if (map[(int)fPlayerY * nMapWidth + (int)fPlayerX] == '#') {
+				fPlayerX -= sinf(fPlayerA) * 5.0f * fElapsedTime;
+				fPlayerY -= cosf(fPlayerA) * 5.0f * fElapsedTime;
+			}
+		}
+		if (GetAsyncKeyState((unsigned short)'S') & 0x8000) {
+			fPlayerX -= sinf(fPlayerA) * 5.0f * fElapsedTime;
+			fPlayerY -= cosf(fPlayerA) * 5.0f * fElapsedTime;
+			if (map[(int)fPlayerY * nMapWidth + (int)fPlayerX] == '#') {
+				fPlayerX += sinf(fPlayerA) * 5.0f * fElapsedTime;
+				fPlayerY += cosf(fPlayerA) * 5.0f * fElapsedTime;
+			}
+		}
+
+		for (int x = 0; x < nScreenWidth; x++) { 
+			float fRayAngle = (fPlayerA - fFOV / 2.0f) + ((float)x / (float)nScreenWidth) * fFOV;
+
+			float fDistanceToWall = 0;
+			bool bHitWall = false;
+			bool bBoundary = false;
+			 
+			float fEyeX = sinf(fRayAngle);
+			float fEyeY = cosf(fRayAngle);
+
+			while (!bHitWall && fDistanceToWall < fDepth)
+			{
+				fDistanceToWall += 0.1f;
+
+				int nTestX = (int)(fPlayerX + fEyeX * fDistanceToWall);
+				int nTestY = (int)(fPlayerY + fEyeY * fDistanceToWall);
+
+				if (nTestX < 0 || nTestX >= nMapWidth || nTestY < 0 || nTestY >= nMapHeight) {
+					bHitWall = true;
+					fDistanceToWall = fDepth;
+				}
+				else 
+				{
+					if (map[nTestY * nMapWidth + nTestX] == '#') {
+						bHitWall = true;
+
+						vector<pair<float, float>> p; 
+						for (int tx = 0; tx < 2; tx++) {
+							for (int ty = 0; ty < 2; ty++) {
+								float vy = (float)nTestY + ty - fPlayerY;
+								float vx = (float)nTestX + tx - fPlayerX;
+								float d = sqrt(vx * vx + vy * vy);
+								float dot = (fEyeX * vx / d) + (fEyeY * vy / d);
+								p.push_back(make_pair(d, dot));
+							}
+
+							sort(p.begin(), p.end(), [](const pair<float, float>& left, const pair<float, float>& right) {return left.first < right.first; });
+							float fBound = 0.01;
+							if (acos(p.at(0).second) < fBound) bBoundary = true;
+							if (acos(p.at(1).second) < fBound) bBoundary = true;
+						}
+					}
+				}	
+			}
+
+			int nCeiling = (float)(nScreenHeight / 2.0) - nScreenHeight / ((float)fDistanceToWall);
+			int nFloor = nScreenHeight - nCeiling;
+ 
+			short nShade = ' ';
+			short Shade = ' ';
+
+			if (fDistanceToWall <= fDepth / 4.0f)					nShade = 0x2588; // PLayer Close;
+			else if (fDistanceToWall < fDepth / 3.0f)				nShade = 0x2593;
+			else if (fDistanceToWall < fDepth / 2.0f)				nShade = 0x2592;
+			else if (fDistanceToWall < fDepth)						nShade = 0x2591;
+			else													nShade = ' ';	 // Player too far 
+			
+			if (bBoundary) nShade = ' ';
+
+			for (int y = 0; y < nScreenHeight; y++) {
+				if (y < nCeiling) {
+					screen[y * nScreenWidth + x] = ' ';
+				}
+				else if (y > nCeiling && y <= nFloor) {
+					screen[y * nScreenWidth + x] = nShade;
+				}else{
+					float b = 1.0f - (((float)y - nScreenHeight / 2.0f) / ((float)nScreenHeight / 2.0f));
+					if (b < 0.25)					Shade = '#'; // PLayer Close;
+					else if (b < 0.5)				Shade = 'x';
+					else if (b < 0.75)				Shade = '.';					/*Floor*/
+					else if (b < 0.9)				Shade = '-';
+					else							Shade = ' ';	 // Player too far 
+					screen[y * nScreenWidth + x] =  Shade;
+				}
+			}
+
+ 		}
+		swprintf_s(screen, 40, L"X=%3.2f, Y=%3.2f, A=%3.2f FPS=%3.2f", fPlayerX, fPlayerY, fPlayerA, 1.0f / fElapsedTime);
+
+		for (int nx = 0; nx < nMapWidth; nx++) {
+			for (int ny = 0; ny < nMapWidth; ny++) {
+				screen[(ny + 1) * nScreenWidth + nx] = map[ny * nMapWidth + nx];
+			}
+		}
+		screen[((int)fPlayerY + 1) * nScreenWidth + (int)fPlayerX] = 'o';
+		screen[nScreenWidth * nScreenHeight - 1] = '\0';
+		WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
+	}
+
+	
+
+
+	
+	return 0;
+}
